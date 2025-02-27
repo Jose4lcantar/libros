ed72b079272b79fa9a695b4b32c9064408bb34d0
Jose <joseam.ti22@utsjr.eu.mx>
2025-02-27 17:16:03 -0600

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig : {
+        apiKey: "AIzaSyBhOkCHT7e7MQ-ePFAyS9FXbbbWTEtGggo",
+        authDomain: "empleados-7ff66.firebaseapp.com",
+        databaseURL: "https://empleados-7ff66-default-rtdb.firebaseio.com",
+        projectId: "empleados-7ff66",
+        storageBucket: "empleados-7ff66.firebasestorage.app",
+        messagingSenderId: "526264582187",
+        appId: "1:526264582187:web:8aa82badc347bf0d6a2a6e"
+      }
+};
